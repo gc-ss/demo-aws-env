3abe291741ba49b93bd42055db6438843683b6ba
Patrick Kuny <pmk62810@gmail.com>
2021-04-22 18:33:52 -0500

    Move TF backend management to bootstrapping

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,32 +24,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-285db723-5211-49ad-840b-ad14c9f72d2e"
-  acl    = "private"
-
-  tags = {
-    Name    = "Terraform State"
-    Managed = "Terraform"
-  }
-}
-
-# Locking state for ensuring multiple developer state applications don't break state.
-resource "aws_dynamodb_table" "terraform" {
-  name         = "terraform"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Managed = "Terraform"
-  }
-}
-
 locals {
   env = terraform.workspace
 }

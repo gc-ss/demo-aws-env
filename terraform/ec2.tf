8a2050b6797df4a58a8c131f7f582a9f855fbc90
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-04-28 12:43:11 -0500

    Add Security Groups, Rules, Prometheus Instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,6 +14,9 @@ resource "aws_instance" "training-vault-01" {
   iam_instance_profile = var.iam_role
   subnet_id            = module.demo.private_subnets[0]
 
+  # Security Groups
+  vpc_security_group_ids = ["${aws_security_group.external_access.id}"]
+
   tags = merge(
     var.tags_mgmt,
     {
@@ -31,10 +34,30 @@ resource "aws_instance" "training-jenkins-01" {
   # Jenkins Bootstrapping
   user_data = file("helpers/jenkins_bootstrap.sh")
 
+  # Security Groups
+  vpc_security_group_ids = ["${aws_security_group.external_access.id}"]
+
   tags = merge(
     var.tags_mgmt,
     {
       Name = "training-jenkins-01"
+    }
+  )
+}
+
+resource "aws_instance" "training-prometheus-01" {
+  ami                  = data.aws_ami.amazon-linux-2.id
+  instance_type        = "t3.micro"
+  iam_instance_profile = var.iam_role
+  subnet_id            = module.demo.private_subnets[0]
+
+  # Security Groups
+  vpc_security_group_ids = ["${aws_security_group.external_access.id}"]
+
+  tags = merge(
+    var.tags_mgmt,
+    {
+      Name = "training-prometheus-01"
     }
   )
 }
--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -1,7 +1,18 @@
-#module "jenkins_sg" {
-#    source = "terraform-aws-modules/security-group/aws"
-#
-#    name = "jenkins-server"
-#    description = "Security Group for Jenkins Frontend"
-#    vpc_id = 
-#}
+resource "aws_security_group" "external_access" {
+  name        = "External Access"
+  description = "Allow access from local network"
+  vpc_id      = module.demo.vpc_id
+
+  tags = var.tags_mgmt
+}
+
+resource "aws_security_group_rule" "local_ip" {
+  type              = "ingress"
+  description       = "Access from External Home IP"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = ["${var.local_ip}"]
+  security_group_id = aws_security_group.external_access.id
+
+}

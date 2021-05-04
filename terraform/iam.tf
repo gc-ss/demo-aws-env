c4a8c74ed673e6c9e08e68799454a83157517e9b
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-05-04 00:50:13 +0000

    Rework IAM roles and policies to be folder-based

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,19 +1,42 @@
-resource "aws_iam_role" "organization_lambda_execution_role" {
-  name = "OrganizationLambdaExecutionRole"
+
+# Lambda: OrganizationPractice Role
+resource "aws_iam_role" "OrganizationPractice_role" {
+  name = "OrganizationPracticeRole"
   path = "/external/"
 
-  assume_role_policy = file("iam_json/lambda_assume_policy.json")
+  assume_role_policy = file("iam_json/OrganizationPractice_role/OrganizationPractice_role_assume.json")
 
   tags = var.tags_mgmt
 }
 
-resource "aws_iam_policy" "organization_lambda_role_policy" {
-  name   = "OrganizationLambdaExcutionPolicy"
-  policy = file("iam_json/organization_lambda_execution_role_policy.json")
+resource "aws_iam_policy" "OrganizationPractice_role_policy" {
+  name   = "OrganizationPracticeRolePolicy"
+  policy = file("iam_json/OrganizationPractice_role/OrganizationPractice_role_policy.json")
 }
 
-resource "aws_iam_policy_attachment" "attach_assume_policy_to_role_lambda" {
-  name       = "attach_assume_policy_to_role_lambda"
-  roles      = ["${aws_iam_role.organization_lambda_execution_role.name}"]
-  policy_arn = aws_iam_policy.organization_lambda_role_policy.arn
+resource "aws_iam_policy_attachment" "OrganizationPractice_role_attach" {
+  name       = "OrganizationPracticeRoleAttach"
+  roles      = ["${aws_iam_role.OrganizationPractice_role.name}"]
+  policy_arn = aws_iam_policy.OrganizationPractice_role_policy.arn
+}
+
+# Lambda: Instance Scheduler Role
+resource "aws_iam_role" "InstanceScheduler_role" {
+  name = "InstanceSchedulerRole"
+  path = "/utilities/"
+
+  assume_role_policy = file("iam_json/InstanceScheduler_role/assume_role_policy.json")
+
+  tags = var.tags_mgmt
+}
+
+resource "aws_iam_policy" "InstanceScheduler_role_policy" {
+  name = "InstanceSchedulerRolePolicy"
+  policy = file("iam_json/InstanceScheduler_role/role_policy.json")
+}
+
+resource "aws_iam_policy_attachment" "InstanceScheduler_role_attach" {
+  name = "InstanceSchedulerRoleAttach"
+  roles = ["${aws_iam_role.InstanceScheduler_role.name}"]
+  policy_arn = aws_iam_policy.InstanceScheduler_role_policy.arn
 }

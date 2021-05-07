a492a62ec29370587c784aaf5afa6769dec8f0fb
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-05-07 21:41:35 +0000

    Switch bootstrap over to amazon-linux-extras

--- terraform/helpers/dev_bootstrap.sh
+++ terraform/helpers/dev_bootstrap.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo yum -y install git
-sudo amazon-linux-extras install ansible2 -y
+yum install -y git
+amazon-linux-extras install ansible2 -y
+amazon-linux-extras install docker -y
 cd /tmp
 git clone https://github.com/pmkuny/demo-aws-env
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-cd dev_instance
+cd /tmp/demo-aws-env/dev_instance/
 ansible-galaxy collection install community.general
 ansible-playbook -i hosts playbook.yml

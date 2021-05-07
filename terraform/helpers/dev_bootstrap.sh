fbc0d3b03d9721f292406017e62d003f0324158a
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-05-07 14:56:38 +0000

    Fixes #4, also adjust dev_instance ansible

--- terraform/helpers/dev_bootstrap.sh
+++ terraform/helpers/dev_bootstrap.sh
@@ -2,6 +2,7 @@
 sudo yum -y install git
 sudo amazon-linux-extras install ansible2 -y
 cd /tmp
-git clone https://github.com/pmkuny/scripts
-cd scripts/dev_instance
+git clone https://github.com/pmkuny/demo-aws-env
+cd dev_instance
+ansible-galaxy collections install community.general
 ansible-playbook -i hosts playbook.yml

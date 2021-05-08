e04471702bcdb60251f38552a9fc0280853b73c8
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2021-05-08 02:33:36 +0000

    Fix cron expressions

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,12 +2,12 @@ resource "aws_cloudwatch_event_rule" "outside_businss_hours" {
     name =     "OutsideBusinessHours"
     description = "Schedule Expression evaluating to overnight"
 
-    schedule_expression = "cron(0 1 * * *)"
+    schedule_expression = "cron(0 1 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_rule" "inside_business_hours" {
     name = "InsideBusinessHours"
     description = "Schedule Expression evaluating to start of day"
 
-    schedule_expression = "cron(0 6 * * *)"
+    schedule_expression = "cron(0 6 * * ? *)"
 }
